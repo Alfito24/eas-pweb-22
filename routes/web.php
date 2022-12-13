5176f083699fceac8da6ed71d8d7d0c193741762
alfito24 <khansaalfito@gmail.com>
2022-12-13 18:44:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
